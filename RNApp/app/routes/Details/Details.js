780b49ecf7c2c524517f1bcf544d85d0e02ab4c1
balibou <benjamin.cherion@gmail.com>
2016-08-09 15:46:32 +0100

    Insert new document (with new data structure)

--- RNApp/app/routes/Details/Details.js
+++ RNApp/app/routes/Details/Details.js
@@ -1,27 +1,84 @@
 import React, { PropTypes } from 'react';
-import { Text, View } from 'react-native';
-import { MeteorListView } from 'react-native-meteor';
+import { Text, View, TextInput } from 'react-native';
+import Meteor, { MeteorListView } from 'react-native-meteor';
 import Loading from '../../components/Loading';
 import styles from './styles';
 
-const Details = ({ detailsReady }) => {
-  if (!detailsReady) {
-    return <Loading />;
+// // const insertNewDocument = (item) =>
+// //   Meteor.call('documents.insert', {
+// //     title: item.nativeEvent.text,
+// //   }, (error, response) => {
+// //     if (error) console.warn(error.reason);
+// //     if (response) console.log(response);
+// //   });
+//
+// const Details = ({ detailsReady, title }) => {
+//   if (!detailsReady) {
+//     return <Loading />;
+//   }
+//
+//   console.log(this);
+//
+//   return (
+//     <View style={styles.container}>
+//       <TextInput
+//         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+//         onChangeText={(text) => this.setState({ text })}
+//         // value={this.state.text}
+//         // onSubmitEditing={insertNewDocument}
+//       />
+//       <MeteorListView
+//         collection="Documents"
+//         renderRow={(document) => <Text>{document.title}</Text>}
+//       />
+//     </View>
+//   );
+// };
+//
+// Details.propTypes = {
+//   detailsReady: PropTypes.bool,
+// };
+//
+// export default Details;
+
+const insertNewDocument = (item) =>
+  Meteor.call('documents.insert', {
+    title: item.nativeEvent.text,
+  }, (error, response) => {
+    if (error) console.warn(error.reason);
+    if (response) console.log(response);
+  });
+
+export default class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
   }
 
-  return (
-    <View style={styles.container}>
-      <MeteorListView
-        contentContainerStyle={styles.list}
-        collection="Documents"
-        renderRow={(detail) => <Text style={styles.item}>{detail.name}</Text>}
-      />
-    </View>
-  );
-};
+  render() {
+    const { detailsReady } = this.props;
+    if (!detailsReady) {
+      return <Loading />;
+    }
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+          onSubmitEditing={insertNewDocument}
+        />
+        <MeteorListView
+          collection="Documents"
+          renderRow={(document) => <Text>{document.title}</Text>}
+        />
+      </View>
+    );
+  }
+}
 
 Details.propTypes = {
   detailsReady: PropTypes.bool,
 };
-
-export default Details;
