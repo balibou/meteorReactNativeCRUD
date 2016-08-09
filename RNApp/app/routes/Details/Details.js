05ef00fe88ca7f72eae995a89bed0adbd8a313c5
balibou <benjamin.cherion@gmail.com>
2016-08-09 16:19:33 +0100

    Remove a document

--- RNApp/app/routes/Details/Details.js
+++ RNApp/app/routes/Details/Details.js
@@ -1,45 +1,8 @@
 import React, { PropTypes } from 'react';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Meteor, { MeteorListView } from 'react-native-meteor';
 import Loading from '../../components/Loading';
 import styles from './styles';
-
-// // const insertNewDocument = (item) =>
-// //   Meteor.call('documents.insert', {
-// //     title: item.nativeEvent.text,
-// //   }, (error, response) => {
-// //     if (error) console.warn(error.reason);
-// //     if (response) console.log(response);
-// //   });
-//
-// const Details = ({ detailsReady, title }) => {
-//   if (!detailsReady) {
-//     return <Loading />;
-//   }
-//
-//   console.log(this);
-//
-//   return (
-//     <View style={styles.container}>
-//       <TextInput
-//         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-//         onChangeText={(text) => this.setState({ text })}
-//         // value={this.state.text}
-//         // onSubmitEditing={insertNewDocument}
-//       />
-//       <MeteorListView
-//         collection="Documents"
-//         renderRow={(document) => <Text>{document.title}</Text>}
-//       />
-//     </View>
-//   );
-// };
-//
-// Details.propTypes = {
-//   detailsReady: PropTypes.bool,
-// };
-//
-// export default Details;
 
 const insertNewDocument = (item) =>
   Meteor.call('documents.insert', {
@@ -48,6 +11,24 @@ const insertNewDocument = (item) =>
     if (error) console.warn(error.reason);
     if (response) console.log(response);
   });
+
+const removeDocument = (document) =>
+    Meteor.call('documents.remove', {
+      _id: document._id,
+    }, (error, response) => {
+      if (error) console.warn(error.reason);
+      if (response) console.log(response);
+    });
+
+const renderRow = (document) =>
+  <View>
+    <Text>{document.title}</Text>
+    <TouchableOpacity style={styles.button} onPress={() => removeDocument(document)}>
+       <Text style={styles.buttonText}>
+         Remove
+       </Text>
+     </TouchableOpacity>
+  </View>;
 
 export default class Details extends React.Component {
   constructor(props) {
@@ -72,7 +53,7 @@ export default class Details extends React.Component {
         />
         <MeteorListView
           collection="Documents"
-          renderRow={(document) => <Text>{document.title}</Text>}
+          renderRow={renderRow}
         />
       </View>
     );
